492ff3b8ba9abb8599f0a2aa074a15f36d372477
Sam-Oladapo Ebenezer <mrsamoladapo@gmail.com>
2022-12-08 02:15:45 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    else:
+        opcode = str(sys.argv[2])
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        if opcode == '+':
+            print("{0} + {1} = {2}".format(a, b, add(a, b)))
+        elif opcode == '-':
+            print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+        elif opcode == '*':
+            print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+        elif opcode == '/':
+            print("{0} / {1} = {2}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
